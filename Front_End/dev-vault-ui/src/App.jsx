56fa16c35576dc9dcffaf527fcb4c7ac2f6e7edb
dd-1710 <divyadurgameesala@gmail.com>
2026-09-01 22:43:14 +0530

    Added default path

--- Front_End/dev-vault-ui/src/App.jsx
+++ Front_End/dev-vault-ui/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import './App.css'
 import { Login } from './pages/Login'
 import { Snippet } from './pages/Snippets'
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
     <Routes>
+       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path='/login' element={<Login/>}></Route>
       <Route element={<ProtectedRoutes/>}>
        <Route path='/snippets' element = {<Snippet/>}/>
